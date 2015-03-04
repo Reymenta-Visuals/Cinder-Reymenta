fb277d4c9165b306e4a4a6d36098e1bb7354006c
brucelane <bruce@batchass.fr>
2015-03-04 15:01:59 +0100

    web sockets wrapper

---
+++ include/WebSocketsWrapper.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "cinder/app/AppNative.h"
+#include "WebSocketClient.h"
+#include "ParameterBag.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+namespace Reymenta 
+{
+
+	typedef std::shared_ptr<class WebSockets> WebSocketsRef;
+
+	class WebSockets 
+	{
+
+	public:
+		WebSockets( ParameterBagRef aParameterBag );
+		static	WebSocketsRef create( ParameterBagRef aParameterBag );
+		void	update();
+		void	setupSender();
+	private:
+		// parameters
+		ParameterBagRef				mParameterBag;
+		// Web socket client
+		void						connect();
+		void						disconnect();
+		WebSocketClient				mClient;
+		void						onConnect();
+		void						onDisconnect();
+		void						onError(std::string err);
+		void						onInterrupt();
+		void						onPing(std::string msg);
+		void						onRead(std::string msg);
+		void						write();
+		std::string					mText;
+
+	};
+}
---
+++ src/WebSocketsWrapper.cpp
@@ -0,0 +1,75 @@
+#include "WebSocketsWrapper.h"
+
+using namespace Reymenta;
+
+WebSockets::WebSockets(ParameterBagRef aParameterBag)
+{
+	mParameterBag = aParameterBag;
+	mClient.addConnectCallback(&WebSockets::onConnect, this);
+	mClient.addDisconnectCallback(&WebSockets::onDisconnect, this);
+	mClient.addErrorCallback(&WebSockets::onError, this);
+	mClient.addInterruptCallback(&WebSockets::onInterrupt, this);
+	mClient.addPingCallback(&WebSockets::onPing, this);
+	mClient.addReadCallback(&WebSockets::onRead, this);
+	connect();
+}
+
+WebSocketsRef WebSockets::create(ParameterBagRef aParameterBag)
+{
+	return shared_ptr<WebSockets>(new WebSockets(aParameterBag));
+}
+void WebSockets::setupSender()
+{
+}
+void WebSockets::update()
+{
+	mClient.poll();
+}
+void WebSockets::connect()
+{
+	mClient.connect("ws://localhost:9002");
+}
+
+void WebSockets::disconnect()
+{
+	mClient.disconnect();
+}
+void WebSockets::onConnect()
+{
+	mText = "Connected";
+	mClient.ping();
+}
+
+void WebSockets::onDisconnect()
+{
+	mText = "Disconnected";
+}
+
+void WebSockets::onError(string err)
+{
+	mText = "Error";
+	if (!err.empty()) {
+		mText += ": " + err;
+	}
+}
+
+void WebSockets::onInterrupt()
+{
+	mText = "Interrupted";
+}
+
+void WebSockets::onPing(string msg)
+{
+	mText = "Ponged";
+	if (!msg.empty()) {
+		mText += ": " + msg;
+	}
+}
+
+void WebSockets::onRead(string msg)
+{
+	mText = "Read";
+	if (!msg.empty()) {
+		mText += ": " + msg;
+	}
+}
